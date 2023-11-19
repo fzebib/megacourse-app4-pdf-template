88c1aaf8e17d1cd1c66bb32328bfdde82fe65aaf
Firas Zebib <firas.zebib@bain.com>
2023-11-19 18:10:04 -0500

    simplify logic for lines

--- main.py
+++ main.py
@@ -15,6 +15,10 @@ for index, row in df.iterrows():
     pdf.cell(w=0, h=12, txt=row['Topic'], align="L", ln=1)
     pdf.line(10, 21, 200, 21)
 
+    for y in range(20, 285, 10):
+        pdf.line(10, y, 200, y)
+
+    # Set Footer
     pdf.ln(265)
     pdf.set_font(family="Times", style="I", size=8)
     pdf.set_text_color(180, 180, 180)
@@ -23,6 +27,10 @@ for index, row in df.iterrows():
     for page in range(row["Pages"] - 1):
         pdf.add_page()
 
+        for y in range(20, 285, 10):
+            pdf.line(10, y, 200, y)
+
+        # Set Footer
         pdf.ln(277)
         pdf.set_font(family="Times", style="I", size=8)
         pdf.set_text_color(180, 180, 180)
